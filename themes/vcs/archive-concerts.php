f021ec6ebf78e6d814dbe1519b1eee3fd1e02a1c
mr-moto <khash615@gmail.com>
2017-08-11 01:59:44 +0900

    season pass and concert structure

--- themes/vcs/archive-concerts.php
+++ themes/vcs/archive-concerts.php
@@ -11,7 +11,8 @@ get_header(); ?>
     <?php 
     $concerts = get_posts(array(
         'post_type' => 'concerts',
-        'numberposts' => 3,
+        'numberposts' => -1,
+        'order' => 'ASC',
         'tax_query' => array(
             array(
                 'taxonomy' => 'concert_type',
@@ -35,13 +36,13 @@ get_header(); ?>
         </div>
     <?php endforeach; wp_reset_postdata()?>
 </section>
-
 <section class="community">
     <h2>community</h2>
     <?php 
     $community_concerts = get_posts(array(
         'post_type' => 'concerts',
-        'numberposts' => 2,
+        'numberposts' => -1,
+        'order' => 'ASC',
         'tax_query' => array(
             array(
                 'taxonomy' => 'concert_type',
@@ -62,7 +63,27 @@ get_header(); ?>
                 <p class="concert-price"><?php the_field('concert_price'); ?></p>
                 <p class="concert-location"><?php the_field('concert_location'); ?></p>
             </div>
+            <div class="concert-popout">
+            </div>
         </div>
     <?php endforeach; wp_reset_postdata()?>
+</section>
+<section class="popout-container">
+    <div class="close-popout">
+        <i class="fa fa-times fa-2x" aria-hidden="true"></i>
+    </div>
+    <div class="popout-content">
+        <div class="popout-main">
+            <p class="concert-title"><?php the_title(); ?></p>
+            <?php the_post_thumbnail(); ?>
+            <p class="concert-date"><?php the_field('concert_date'); ?></p>
+            <p class="concert-price"><?php the_field('concert_price'); ?></p>
+            <p class="concert-location"><?php the_field('concert_location'); ?></p>
+        </div>
+        <div class="popout-buy-tickets">
+            <div style="width:100%; text-align:left;"><iframe src="//eventbrite.com/tickets-external?eid=36690852327&ref=etckt" frameborder="0" height="275" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:100%; text-align:left;" ><a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none;" target="_blank" href="http://www.eventbrite.com/">Powered by Eventbrite</a></div></div>
+        </div>
+        <div class="popout-more"></div>
+    </div>
 </section>
 <?php get_footer(); ?>
--- themes/vcs/front-page.php
+++ themes/vcs/front-page.php
@@ -6,5 +6,49 @@
  */
 
 get_header(); ?>
-
+<section class="season-pass">
+    <div class="season-image-container">
+        <img src="<?php the_field('hp_season_image'); ?>" alt="">
+    </div>
+    <div class="season-copy">
+        <h2><?php the_field('hp_season_header'); ?></h2>
+        <p><?php the_field('hp_season_copy'); ?></p>
+    </div>
+    <a class="season-button button" href="#">Buy Season Pass</a>
+</section>
+<section class="concert">
+    <h2>Concerts</h2>
+    <div class="concert-carousel">
+        <?php $args = array (
+            'post_type' => 'concerts',
+            'order' => 'DCS',
+            'post_status' => 'publish',
+            'posts_per_page' => -1
+        );
+        $concerts_list = get_posts( $args );
+        foreach( $concerts_list as $post) :?>
+            <div class="concert-post">
+                <div class="concert-post-head">
+                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                    <p class="concert-title"><?php the_title(); ?></p>
+                    <p class="concert-date"><?php the_field('concert_date'); ?></p>
+                </div>
+                <p><?php the_field('concert_location'); ?></p>
+                <p><?php the_excerpt(); ?></p>
+            </div>
+        <?php endforeach; wp_reset_postdata(); ?>
+    </div>
+    <a class="concert-view-more button" href="">View More</a>
+    <!-- view more buttons -->
+</section>
+<section class="blog">
+    <h2>From The Blog</h2>
+    <div class="blog-image-container"></div>
+    <!-- blog title -->
+    <!-- blog content -->
+</section>
+<section class="buy-donate">
+    <a class="buy-tickets button" href=""></a>
+    <a class="donate button" href=""></a>
+</section>
 <?php get_footer(); ?>
