b83aec1295bc89b2b326a1853f71e9c0ae2ffff2
mr-moto <khash615@gmail.com>
2017-09-17 03:32:57 +0900

    .

--- themes/vcs/header.php
+++ themes/vcs/header.php
@@ -58,13 +58,8 @@
 					<div class="main-menu-container">
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</div>
-					<div class="buy-donate-container">
-						<div class="button-container">
-							<a class="buy-tickets button-green button" href="<?php echo esc_url( home_url( '/concerts	' ) ); ?>">Buy Tickets</a>
-    						<a class="donate button-red button" href="＃">Donate</a>
-						</div>
-					</div>
 
+					<div class="body-gradient"></div>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
